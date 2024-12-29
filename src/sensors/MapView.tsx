63f5aeb3d5398b6a4e875e2e40f589a2cdf642ab
araamz <araamzaremehrjardi@gmail.com>
2024-12-28 22:48:42 -0800

    Readding lost files

--- src/sensors/MapView.tsx
+++ src/sensors/MapView.tsx
@@ -1,19 +1,14 @@
 import { MapContainer, TileLayer } from "react-leaflet";
-import SensorSelection from "../components/SensorSelection.tsx";
 import useLidarMetadata, {
-  ILidarMetadata,
-  ILidarMetadataItem,
 } from "../hooks/backend/metadataService/useLidarMetadata";
 import { useEffect, useState } from "react";
-import FrostedPagination from "../components/FrostedPagination.tsx";
-import Loader from "../components/Loader.tsx";
-import FormatSelector from "../components/FormatSelector.tsx";
 
 export default function MapView() {
   const formatOptions = ["ros", "pcap"];
   const [page, setPage] = useState<number>(1);
   const [format, setFormat] = useState<string>("ros");
   const { isPending, data, error } = useLidarMetadata(page, 10);
+  const [expanded, setExpanded] = useState<boolean>(false);
 
   useEffect(() => {
     if (data) console.log(data);
@@ -36,20 +31,19 @@ export default function MapView() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer>
-      <div className="absolute bottom-0 left-0 p-4 bg-white/70 backdrop-blur w-full gap-3 flex flex-col lg:left-0 lg:top-0 lg:w-[3/4] lg:max-w-[350px]">
+      <div className='absolute bottom-0 left-0 p-4 bg-white/70 backdrop-blur w-full gap-3 flex flex-col lg:left-0 lg:top-0 lg:w-[3/4] lg:max-w-[350px] drop-shadow'>
+      </div>
+    </div>
+  );
+}
+
+/*
         <Loader isLoading={isPending}>
           <>
             <div>
-              <FormatSelector
-                formats={formatOptions}
-                selectedFormat={format}
-                setFormat={(format) => setFormat(format)}
-              />
+
             </div>
             <div className="inline-flex flex-row overflow-x-auto gap-4 max-w-full lg:grow lg:flex-col">
-              {data?.items.map((sensor: ILidarMetadataItem) => (
-                <SensorSelection key={sensor.lidar_id} sensor={sensor} />
-              ))}
             </div>
             <FrostedPagination
               setPage={(pageNumber) => setPage(pageNumber)}
@@ -61,7 +55,4 @@ export default function MapView() {
             />
           </>
         </Loader>
-      </div>
-    </div>
-  );
-}
+*/
